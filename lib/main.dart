072057cee7e8997b5c619b887ff2eb5f0abea88f
aveepsit <aveepsitchowdhury@gmail.com>
2020-09-01 15:49:32 +0530

    added scaffold and title
    Signed-off-by: aveepsit <aveepsitchowdhury@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: EightBall(),
       ),
     );
+
+class EightBall extends StatefulWidget {
+  @override
+  _EightBallState createState() => _EightBallState();
+}
+
+class _EightBallState extends State<EightBall> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text(
+          'Ask Me Anything',
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+        backgroundColor: Colors.blue[800],
+      ),
+      body: Container(),
+    );
+  }
+}
